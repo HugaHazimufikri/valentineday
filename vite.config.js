6c207c5804d216a8303c61592bac931bce18df05
Huga Hazimulfikri Nawawi <huga36360@gmail.com>
2025-02-16 05:46:01 +0700

    Fix build issues for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/valentineday/", // Sesuai nama repository GitHub
-})
+  base: "/valentineday/", // Sesuai nama repo di GitHub
+  plugins: [react()]
+});
